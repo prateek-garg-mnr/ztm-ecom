17412a5f412be98da6a5a631503d580384631fc8
prateek-garg-mnr <69619674+prateek-garg-mnr@users.noreply.github.com>
2021-04-28 14:05:07 +0530

    add collection preview, item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import { Route, Switch } from "react-router-dom";
-import './App.css';
-import Homepage from "./components/homepage/homepage.component";
-
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.components";
+import "./App.css";
 function App() {
   return (
 		<div>
 			<Route exact path="/" component={Homepage} />
-			<Route exact path="/topics" />
-			<Route exact path="/topics/:topicId" />
+			<Route exact path="/shop" component={ShopPage} />
 		</div>
 	);
 }
